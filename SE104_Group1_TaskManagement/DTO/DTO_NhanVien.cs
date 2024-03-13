722993c262911febab7a540d2ac476a2aecc6e43
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2024-03-13 11:26:21 +0700

    Added DTO, DAL

---
+++ SE104_Group1_TaskManagement/DTO/DTO_NhanVien.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace DTO
+{
+    public class DTO_NhanVien
+    {
+        private string _manv;
+        private string _tennv;
+        private string _email;
+        private string _phone;
+        private string _ngaysinh;
+        private int _level;
+        private string _macm;
+        private string _ghichu;
+
+        public DTO_NhanVien (string manv = "", string tennv = "", string email = "", string phone = "", string ngaysinh = "", int level = -1, string macm = "", string ghichu = "")
+        {
+            _manv = manv;
+            _tennv = tennv;
+            _email = email;
+            _phone = phone;
+            _ngaysinh = ngaysinh;
+            _level = level;
+            _macm = macm;
+            _ghichu = ghichu;
+        }
+
+        public string MANV { get { return _manv;} set { _manv = value; } }
+        public string TENNV { get { return _tennv;} set {  _tennv = value; } }
+        public string EMAIL { get { return _email; } set { _email = value; } }
+        public string PHONE { get { return _phone; } set { _phone = value; } }
+        public int LEVEL { get { return _level; } set { _level = value; } }
+        public string NGAYSINH { get { return _ngaysinh; } set { _ngaysinh = value; } }
+        public string MACM { get { return _macm; } set { _macm = value; } }
+        public string GHICHU { get { return _ghichu; } set { _ghichu = value; } }
+
+    }
+}
