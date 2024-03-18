97659d760880c4cc5a073729fcfb46234fb3e271
PhongNguyen0511 <162393311+PhongNguyen0511@users.noreply.github.com>
2024-03-18 17:49:42 +0700

    Update SqlQuery.sql

--- SE104_Group1_TaskManagement/SqlQuery.sql
+++ SE104_Group1_TaskManagement/SqlQuery.sql
@@ -61,3 +61,54 @@ CREATE TABLE CONGVIEC
 	YCDINHKEM	VARCHAR(20),
 	TEPDINHKEM	VARCHAR(50),
 );
+--------------------------------------------------------------------------------
+--TẠO TRIGGER CHO ĐIỀU KIỆN NGÂN SÁCH
+CREATE OR ALTER TRIGGER Check_NGANSACH
+ON DUAN
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    -- Khai báo các biến
+    DECLARE @MALSK CHAR(10);
+    DECLARE @MONEYMIN INT;
+    DECLARE @MONEYMAX INT;
+    DECLARE @TENLSK NVARCHAR(100);
+
+    -- Lặp qua các hàng được chèn hoặc cập nhật
+    DECLARE cur CURSOR FOR
+    SELECT i.MALSK
+    FROM inserted i;
+
+    OPEN cur;
+    FETCH NEXT FROM cur INTO @MALSK;
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        -- Lấy giá trị MONEYMIN, MONEYMAX và TENLSK từ bảng LOAISK tương ứng với MALSK
+        SELECT @MONEYMIN = MONEYMIN, @MONEYMAX = MONEYMAX, @TENLSK = TENLSK
+        FROM LOAISK
+        WHERE MALSK = @MALSK;
+
+        -- Kiểm tra điều kiện và nếu không thỏa mãn, phát sinh lỗi
+        IF EXISTS (
+            SELECT 1
+            FROM inserted i
+            WHERE i.NGANSACH > @MONEYMAX OR i.NGANSACH < @MONEYMIN
+        )
+        BEGIN
+            DECLARE @ErrorMessage NVARCHAR(100);
+            SET @ErrorMessage = N'Ngân sách phải nằm trong khoảng ' + CAST(@MONEYMIN AS NVARCHAR(10)) + N' và ' + CAST(@MONEYMAX AS NVARCHAR(10)) + N' cho sự kiện ' + @TENLSK;
+            RAISERROR(@ErrorMessage, 16, 1);
+            ROLLBACK TRANSACTION;
+            CLOSE cur;
+            DEALLOCATE cur;
+            RETURN;
+        END;
+
+        FETCH NEXT FROM cur INTO @MALSK;
+    END;
+
+    CLOSE cur;
+    DEALLOCATE cur;
+END;
+----------------------------------------------------------------------------
