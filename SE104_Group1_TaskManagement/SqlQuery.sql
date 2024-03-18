683ad29a98953f71b7be7c9f63e6fc67ed6f9e62
PhongNguyen0511 <162393311+PhongNguyen0511@users.noreply.github.com>
2024-03-19 03:06:18 +0700

    Update SqlQuery.sql

--- SE104_Group1_TaskManagement/SqlQuery.sql
+++ SE104_Group1_TaskManagement/SqlQuery.sql
@@ -112,3 +112,82 @@ BEGIN
     DEALLOCATE cur;
 END;
 ----------------------------------------------------------------------------
+-- TÌNH TRẠNG DỰ ÁN
+ALTER TABLE DUAN
+ADD TINHTRANG varchar(20);
+
+ALTER TABLE DUAN ADD CONSTRAINT Check_TinhTrang CHECK (TINHTRANG in ('Completed','On-going','Delayed','Canceled'))
+
+-- NGÂN SÁCH CÔNG VIỆC
+ALTER TABLE CONGVIEC ADD CONSTRAINT Check_NganSachCV CHECK (NGANSACH >=0) 
+
+-- KIỂM TRA TỔNG NGÂN SÁCH CÔNG VIỆC
+CREATE OR ALTER TRIGGER Check_CongViec_Ngansach
+ON DUAN
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    -- Khai báo biến để lưu trữ NGANSACH của DUAN
+    DECLARE @DuanNgansach INT;
+
+    -- Lấy NGANSACH của DUAN
+    SELECT @DuanNgansach = NGANSACH FROM DUAN WHERE MADA IN (SELECT MADA FROM inserted);
+
+    -- Kiểm tra tổng NGANSACH của CÔNG VIỆC so với NGANSACH của DỰ ÁN
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        INNER JOIN (
+            SELECT MADA, SUM(NGANSACH) AS TotalNgansach
+            FROM CONGVIEC
+            GROUP BY MADA
+        ) cv ON i.MADA = cv.MADA
+        WHERE cv.TotalNgansach > @DuanNgansach
+    )
+    BEGIN
+        DECLARE @ErrorMessage NVARCHAR(100);
+        SET @ErrorMessage = N'Tổng ngân sách cho công việc không được vượt quá ' + CAST(@DuanNgansach AS NVARCHAR(10));
+        RAISERROR (@ErrorMessage, 16, 1);
+        ROLLBACK TRANSACTION;
+    END;
+END;
+
+-- KIỂM TRA NGÂN SÁCH ĐÃ DÙNG SO VỚI NGÂN SÁCH CÔNG VIỆC
+CREATE OR ALTER TRIGGER Check_CongViec_Ngansach_Dung
+ON CONGVIEC
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    -- Kiểm tra ngân sách đã dùng so với ngân sách được cấp cho công việc
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        WHERE i.DADUNG > i.NGANSACH
+           OR i.DADUNG < 0
+    )
+    BEGIN
+        RAISERROR (N'Không được dùng quá ngân sách được cấp cho công việc', 16, 1);
+        ROLLBACK TRANSACTION;
+    END;
+END;
+
+-- KIỂM TRA CÔNG VIỆC ĐÚNG CHUYÊN MÔN CỦA NHÂN VIÊN
+CREATE OR ALTER TRIGGER Check_PhanCong_ChuyenMon
+ON CONGVIEC
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    -- Kiểm tra phân công công việc đúng với chuyên môn của nhân viên
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        INNER JOIN NHANVIEN nv ON i.MANV = nv.MANV
+        INNER JOIN PHANCONG p ON i.MACV = p.MACV
+        WHERE nv.MACM <> i.MACM
+    )
+    BEGIN
+        RAISERROR (N'Nhân viên phải được phân công công việc đúng với chuyên môn của mình', 16, 1);
+        ROLLBACK TRANSACTION;
+    END;
+END;
+
