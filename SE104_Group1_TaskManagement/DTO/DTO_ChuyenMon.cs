e60edc2c9d8792709c23a5de1fac8524c96dddbd
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2024-03-20 10:39:12 +0700

    update chuyenmon, loaisk

--- SE104_Group1_TaskManagement/DTO/DTO_ChuyenMon.cs
+++ SE104_Group1_TaskManagement/DTO/DTO_ChuyenMon.cs
@@ -10,14 +10,18 @@ namespace DTO
     {
         string _macm;
         string _cm;
-        public DTO_ChuyenMon(string macm="", string cm = "")
+        string _inshort;
+        public DTO_ChuyenMon(string macm="", string cm = "", string inshort = "")
         {
             MACM = macm;
             TENCM = cm;
+            _inshort = inshort;
         }    
         public string MACM
         { get { return _macm; } set { _macm = value; } }
         public string TENCM
         { get { return _cm; } set { _cm = value; } }
+        public string INSHORT
+        { get { return _inshort; } set { _inshort = value; } }
     }
 }
--- SE104_Group1_TaskManagement/DTO/DTO_LoaiSK.cs
+++ SE104_Group1_TaskManagement/DTO/DTO_LoaiSK.cs
@@ -12,13 +12,15 @@ namespace DTO
         string _tenlsk;
         long _min;
         long _max;
+        string _inshort;
 
-        public DTO_LoaiSK(string malsk="", string tenlsk="", long min=-1, long max=-1)
+        public DTO_LoaiSK(string malsk="", string tenlsk="", long min=-1, long max=-1, string inshort = "")
         {
             _malsk = malsk;
             _tenlsk = tenlsk;
             _min = min;
             _max = max;
+            _inshort = inshort;
         }
 
         public string MALSK
@@ -29,5 +31,7 @@ namespace DTO
         { get { return _min; } set { _min = value; } }
         public long MAX
         { get { return _max; } set { _max = value; } }
+        public string INSHORT
+        { get { return _inshort; } set { _inshort = value; } }
     }
 }
