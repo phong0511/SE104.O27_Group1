d4831207aa86ab1748abb70b1adbe2792d744afa
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2024-03-23 10:14:59 +0700

    DTO and DAL PHANCONG

--- SE104_Group1_TaskManagement/DTO/DTO_PhanCong.cs
+++ SE104_Group1_TaskManagement/DTO/DTO_PhanCong.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    internal class DTO_PhanCong
+    public class DTO_PhanCong
     {
+        string _manv;
+        string _macv;
+
+        public DTO_PhanCong(string manv = "", string macv = "")
+        {
+            _macv = macv;
+            _manv = manv;
+        }
+
+        public string MANV { get { return _manv; } set { _manv = value; } }
+        public string MACV { get { return _macv; } set { _macv = value; } }
     }
 }
