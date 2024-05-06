19d98d66a7a5a0a9d3e37dc97e97a1d944630cce
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2024-05-06 11:38:43 +0700

    GUI NhanVien, login, updateandaddnhanvien done

--- SE104_Group1_TaskManagement/GUI/EmployeesWindow.xaml.cs
+++ SE104_Group1_TaskManagement/GUI/EmployeesWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using BUS;
+using DAL;
+using DTO;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,17 +25,33 @@ namespace GUI
     /// </summary>
     public partial class EmployeesWindow : Window
     {
+        public static DTO_NhanVien crnNV = new DTO_NhanVien();
+        BUS_NhanVien nvManager = new BUS_NhanVien();
+        BUS_TaiKhoan tkManager = new BUS_TaiKhoan();
+        BindingList<DTO_NhanVien> members = new BindingList<DTO_NhanVien>();
         public EmployeesWindow()
         {
             InitializeComponent();
-
+            this.WindowState = WindowState.Maximized;
+            crnNV = nvManager.GetByID(LoginWindow.crnUser.MANV);
+            if (crnNV.MANV != "") username.Text = crnNV.TENNV;
             var converter = new BrushConverter();
-            ObservableCollection<Employee> members = new ObservableCollection<Employee>();
 
-            members.Add(new Employee { ID=1, EmployeeCode = "EP1", EmployeeName = "Nguyen Lam Thanh Triet", Gender="Nam", Technique="Software Engineer", Level="1", Email = "trietn61@gmail.com", Phone = "0985-825-804", Note="None" });
-           
 
+            //  members.Add(new Employee { ID=1, EmployeeCode = "EP1", EmployeeName = "Nguyen Lam Thanh Triet", Gender="Nam", Technique="Software Engineer", Level="1", Email = "trietn61@gmail.com", Phone = "0985-825-804", Note="None" });
+
+            members = nvManager.GetAllData();
             membersDataGrid.ItemsSource = members;
+        }
+        private void Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddAndUpdateEmployee addDialog = new AddAndUpdateEmployee();
+            bool? res = addDialog.ShowDialog();
+            if (res != null && res == true)
+            {
+                members = nvManager.GetAllData();
+                membersDataGrid.ItemsSource = members;
+            }
         }
         private bool IsMaximize = false;
         private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -101,17 +122,76 @@ namespace GUI
             }
         }
 
-    }
-    public class Employee
-    {
-        public int ID { get; set; }
-        public string EmployeeCode { get; set; }
-        public string EmployeeName { get; set; }
-        public string Gender { get; set; }
-        public string Technique { get; set; }
-        public string Level { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Note { get; set; }
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button button = sender as System.Windows.Controls.Button;
+            if (button != null)
+            {
+                // Find the DataGridRow that contains the clicked button
+                DataGridRow row = FindVisualParent<DataGridRow>(button);
+                if (row != null)
+                {
+                    // Access the data item behind the row
+                    DTO_NhanVien? item = row.Item as DTO_NhanVien;
+                    if (item != null)
+                    {
+                        AddAndUpdateEmployee updateDialog = new AddAndUpdateEmployee(item);
+                        bool? res = updateDialog.ShowDialog();
+                        if (res != null && res == true)
+                        {
+                            members = nvManager.GetAllData();
+                            membersDataGrid.ItemsSource = members;
+                        }
+                    }
+
+                    // Do something with the item...
+                }
+            }
+        }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                // Find the DataGridRow that contains the clicked button
+                DataGridRow row = FindVisualParent<DataGridRow>(button);
+                if (row != null)
+                {
+                    // Access the data item behind the row
+                    DTO_NhanVien? item = row.Item as DTO_NhanVien;
+                    if (item != null)
+                    {
+                        (bool, string) res = nvManager.DeleteByID(item);
+                        if (res.Item1 == true)
+                        {
+                            MessageBox.Show(res.Item2);
+                            members = nvManager.GetAllData();
+                            membersDataGrid.ItemsSource = members;
+                        }
+                        else
+                        {
+                            MessageBox.Show(res.Item2);
+                        }
+
+                    }
+                }
+
+                // Do something with the item...
+            }
+        }
+
+        private T FindVisualParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+            if (parentObject == null)
+                return null;
+
+            if (parentObject is T parent)
+                return parent;
+            else
+                return FindVisualParent<T>(parentObject);
+        }
+
+        
     }
 }
