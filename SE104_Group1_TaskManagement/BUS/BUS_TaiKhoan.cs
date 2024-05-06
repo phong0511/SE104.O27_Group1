2e6607d81e1037152fcceb2fd99a734377ba9aef
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2024-05-06 11:38:03 +0700

    #Fix BUS_NhanVien

--- SE104_Group1_TaskManagement/BUS/BUS_TaiKhoan.cs
+++ SE104_Group1_TaskManagement/BUS/BUS_TaiKhoan.cs
@@ -14,22 +14,23 @@ namespace BUS
         //--Duoc goi khi event handling cua button dang nhap duoc goi
         //--Kiem tra tai khoan co ton tai hay khong, neu ton tai thi tra ve mot doi tuong cua lop DTO_NhanVien
         //
-        public DTO_NhanVien Login(DTO_TaiKhoan tk)
+        DAL_TaiKhoan dalTK = new DAL_TaiKhoan();
+        public DTO_TaiKhoan Login(DTO_TaiKhoan tk)
         {
-            return DatabaseAccess.CheckLogicDTO(tk);
+            return dalTK.CheckLogicDTO(tk);
             //Tra ve mot doi tuong cua lop DTO_NhanVien neu tk co ton tai
             //Tra ve mot doi tuong thuoc lop DTO_NhanVien co ma nhan vien = -1 neu tai khoan khong ton tai
         }
 
-        public DTO_NhanVien Register(DTO_TaiKhoan tk)
+        public string Register(DTO_TaiKhoan tk)
         {
-            return DatabaseAccess.TaoMoiTaiKhoan(tk); //Chuyen ham tao moi tai khoan thanh static 
+            return dalTK.TaoMoiTaiKhoan(tk); //Chuyen ham tao moi tai khoan thanh static 
             //Tra ve mot instance cua DTO_NhanVien voi cac gia tri da duoc khoi tao
         }
 
-        public DTO_NhanVien ChangeInfo(string email, string oldPass, string newPass) 
+        public (string, DTO_TaiKhoan) ChangeInfo(string email, string oldPass, string newPass) 
         {
-            return DatabaseAcces.DoiMatKhau(email, oldPass, newPass);
+            return (dalTK.ChangePassword(email, oldPass, newPass));
             //Tra ve instance moi cua DTO_NhanVien
         }
     }
